ea91db346219b648ef7dc17a07def6c78092276a
Daniel <rauddaniel@yahoo.com>
2019-03-26 12:15:56 -0400

    including the time until the next episode

--- src/app/views/episodes/episodes.component.ts
+++ src/app/views/episodes/episodes.component.ts
@@ -14,6 +14,10 @@ episodes: any;
 seasons: any;
 seasonClicked = '';
 clicked2 = false;
+time: Date = new Date();
+diff: any;
+date: any;
+nextEpisode = '';
 
   constructor(private route: ActivatedRoute, private service: ServiceService) { }
 
@@ -32,20 +36,41 @@ clicked2 = false;
     // (2) To get the seasons when click on button of the show
     this.service.getSeasons(this.showId).subscribe( search3 => {
       this.seasons = search3;
-      console.log(this.seasons);
+     // console.log(this.seasons);
     });
 
   }
 
   // (3) Get the episodes of a certain season
-  showEpisodes(season){
+  showEpisodes(season) {
   this.seasonClicked = season.number;
   this.clicked2 = !this.clicked2;
 
   this.service.getEpisodes(this.showId).subscribe( search4 => {
     this.episodes = search4;
-    console.log(this.episodes);
+    // console.log(this.episodes);
+    console.log('Break point 1');
+    for (let i = 0; i < this.episodes.length; i++) {
+      const day = this.episodes[i].airdate;
+      const t = this.episodes[i].airtime;
+      this.date = new Date(day + 'T' + t);
+      this.diff = this.date - Date.now();
+      if (this.diff > 0) {
+        const sec = Math.floor((this.diff) / 1000);
+        const days = Math.floor(sec / 86400);
+        const hours = Math.floor((sec % 86400) / 3600) ;
+        const minutes = Math.floor(((sec % 86400) % 3600) / 60);
+        const seconds = ((sec % 86400) % 3600) % 60;
+        this.nextEpisode = 'Next Episode: ' + this.episodes[i].airdate + ' ' + this.episodes[i].airtime + ' Time left: ' +
+          'days: ' + days + ' hours: ' + hours + ' minutes: ' + minutes + ' seconds: ' + seconds;
+        break;
+      }
+    }
       });
+
+
+
+
 
   }
 
